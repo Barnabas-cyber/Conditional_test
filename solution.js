1c7d0a4f4b3204cbefd78718dd797e46ebde88cd
Barnbas-cyber <moloibarnabas@gmail.com>
2022-08-29 20:15:39 +0200

    final commit

--- solution.js
+++ solution.js
@@ -1,13 +1,13 @@
 //program to check whethers a number is even or odd
-
-//take input from user
-let number = prompt("Enter number >>>");
-
-//check if the number is odd or even
-if (number % 2) {
-    console.log("The number is even.");
-}
-// the number is odd 
-else {
-    console.log("The number is odd");
+for (number = 0; number < 5; number++) {
+    //take input from user
+    let number = prompt("Enter number >>>");
+    //check if the number is odd or even
+    if (number % 2 == 0) {
+        console.log("The number is even.");
+    }
+    // the number is odd 
+    else {
+        console.log("The number is odd");
+    }
 }
