3c505eb82bc37af196b7377765f34d4dce8e2e0b
Barnbas-cyber <moloibarnabas@gmail.com>
2022-08-29 10:10:46 +0200

    scripting

---
+++ solution.js
@@ -0,0 +1,13 @@
+//program to check whethers a number is even or odd
+
+//take input from user
+let number = prompt("Enter number >>>");
+
+//check if the number is odd or even
+if (number % 2) {
+    console.log("The number is even.");
+}
+
+
+//execute the script 
+console.log();
